b2f8a843d1264c4bbfa08b2685f98836b0f8a0cf
Manav Bokinala <manav.bokinala@gmail.com>
2017-08-10 20:47:08 -0700

    Changed the server.js file to include 'Sincerely, ...' blurb

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ hbs.registerHelper('screamIt', (text) => {
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website'
+    welcomeMessage: 'Welcome to my website - Sincerely, Manav'
   });
 });
 
